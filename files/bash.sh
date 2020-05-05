390699d99a9885bb26e01f3d5befa20b4e197f2d
Tim Ron <60440013+t1mron@users.noreply.github.com>
2020-05-05 13:58:46 +0300

    Update bash.sh

--- files/bash.sh
+++ files/bash.sh
@@ -4,6 +4,6 @@
 export DOTNET_ROOT=$HOME/dotnet-arm64
 export PATH=$PATH:$HOME/dotnet-arm64
 
-export GOPATH=\$HOME/gopath
-export PATH=\$GOPATH:\$GOPATH/bin:\$PATH
+export GOPATH=$HOME/gopath
+export PATH=$GOPATH:$GOPATH/bin:$PATH
 # -- user customizations -------------------------------------------------------
