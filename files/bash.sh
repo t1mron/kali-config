ad5192d33a10ce4da7cfb58fe00dbb591295ebdd
Tim Ron <timron@tuta.io>
2020-05-08 11:31:45 +0300

    Update bash.sh

--- files/bash.sh
+++ files/bash.sh
@@ -3,7 +3,4 @@
 # set .NET Core SDK and Runtime path
 export DOTNET_ROOT=$HOME/dotnet-arm64
 export PATH=$PATH:$HOME/dotnet-arm64
-
-export GOPATH=$HOME/gopath
-export PATH=$GOPATH:$GOPATH/bin:$PATH
 # -- user customizations -------------------------------------------------------
