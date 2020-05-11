e2cbb49d4231da1a252f83136430aaa35dd28093
Tim Ron <timron@tuta.io>
2020-05-11 12:32:16 +0300

    Update bash.sh

--- files/bash.sh
+++ files/bash.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 # -- user customizations -------------------------------------------------------
+
+# -- functions ----------------------
+function config_pull()
+	{
+	cd ~/git/linux_config && git pull;
+	sudo cp ~/git/linux_config/files/{antigen.zsh,.zshrc,.p10k.zsh,.tmux.conf.local,bash.sh,.vimrc} ~/
+	sudo cp ~/git/linux_config/files/hostname /etc/;
+	sudo cp ~/git/linux_config/files/sshd_config /etc/ssh/;
+	sudo cp ~/git/linux_config/files/jail.local /etc/fail2ban/;	
+	}
+
+# -- aliases ----------------------
+command -v lsd &> /dev/null && alias ls='lsd --group-dirs first'
+
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+alias python='python3.8'
